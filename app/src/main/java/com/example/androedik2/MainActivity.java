fa5536be26114a209f71c496f4fe870af7463726
Papshev Nikita <nikitapap@yandex.ru>
2023-10-02 15:26:31 +0300

    add featureq

--- app/src/main/java/com/example/androedik2/MainActivity.java
+++ app/src/main/java/com/example/androedik2/MainActivity.java
@@ -3,12 +3,83 @@ package com.example.androedik2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
-
+    int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button delete = findViewById(R.id.DeleteButton);
+        Button add = findViewById(R.id.AddButton);
+        Button edit = findViewById(R.id.EditButton);
+
+        ArrayList<String> myStringArray = new ArrayList<String>();
+        ArrayAdapter<String> TextAdapter =
+                new ArrayAdapter(this, android.R.layout.simple_list_item_1, myStringArray);
+        ListView textList = findViewById(R.id.textList);
+
+        textList.setAdapter(TextAdapter);
+        EditText editText = findViewById(R.id.EditText);
+
+        textList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                index = i;
+            }
+        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!myStringArray.isEmpty())
+                {
+                    myStringArray.remove(index);
+                }
+                index = 0;
+
+                TextAdapter.notifyDataSetChanged();
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myStringArray.add(editText.getText().toString());
+                myStringArray.sort(new Comparator<String>() {
+                    @Override
+                    public int compare(String s, String t1) {
+                        return s.compareTo(t1);
+                    }
+                });
+                editText.setText("");
+                TextAdapter.notifyDataSetChanged();
+            }
+        });
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myStringArray.set(index, editText.getText().toString());
+                myStringArray.sort(new Comparator<String>() {
+                    @Override
+                    public int compare(String s, String t1) {
+                        return s.compareTo(t1);
+                    }
+                });
+                editText.setText("");
+                TextAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
