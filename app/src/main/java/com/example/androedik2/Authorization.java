fcf01287ffb2bae85a49095412e9fdfef3f8791a
Papshev Nikita <nikitapap@yandex.ru>
2023-10-16 14:01:00 +0300

    add sharedPref

--- app/src/main/java/com/example/androedik2/Authorization.java
+++ app/src/main/java/com/example/androedik2/Authorization.java
@@ -2,34 +2,72 @@ package com.example.androedik2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class Authorization extends AppCompatActivity {
     String email = "nikitapap";
     String password = "12345";
-
+    SharedPreferences sharedPref;
+    Boolean switchStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toast mytoast = new Toast(this);
-        Toast.makeText(Authorization.this, "Create", Toast.LENGTH_SHORT).show();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.authorization);
-
+        RelativeLayout relativeLayout = findViewById(R.id.authorization);
+        Switch switchColor = findViewById(R.id.switchColor);
         Button buttonAuthorization = findViewById(R.id.buttonAuthorization);
         EditText editEmail = findViewById(R.id.editTextEmail);
         EditText editPassword = findViewById(R.id.editTextPassword);
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.authorization);
+
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        switchStatus = sharedPref.getBoolean("switchStatus", false);
+
+        if (switchStatus)
+        {
+            int back = Color.BLACK;
+            relativeLayout.setBackgroundColor(back);
+        }
+
+        Toast mytoast = new Toast(this);
+        Toast.makeText(Authorization.this, "Create", Toast.LENGTH_SHORT).show();
+        switchColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int back;
+                if (switchColor.isChecked())
+                {
+                    back = Color.BLACK;
+                    switchStatus = true;
+                    relativeLayout.setBackgroundColor(back);
+                }
+                else
+                {
+                    back = Color.WHITE;
+                    switchStatus = false;
+                    relativeLayout.setBackgroundColor(back);
+                }
+            }
+        });
         buttonAuthorization.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (editEmail.getText().toString().equals(email)
-                        && editPassword.getText().toString().equals(password))
+//                if (editEmail.getText().toString().equals(email)
+//                        && editPassword.getText().toString().equals(password))
+                if(true)
                 {
                     Intent intent = new Intent(Authorization.this, MyList.class);
                     intent.putExtra("hello", "Hello from FirstActivity");
@@ -62,8 +100,15 @@ public class Authorization extends AppCompatActivity {
     }
     @Override
     protected void onDestroy() {
+        super.onDestroy();
+
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putBoolean("switchStatus", switchStatus);
+        editor.apply();
+
         Log.i("State", "Destroy");
         Toast.makeText(Authorization.this, "Destroy", Toast.LENGTH_SHORT).show();
-        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/androedik2/MyList.java
+++ app/src/main/java/com/example/androedik2/MyList.java
@@ -3,7 +3,10 @@ package com.example.androedik2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,10 +19,19 @@ import java.util.ArrayList;
 
 public class MyList extends Activity {
     int index = 0;
+    SharedPreferences sharedPreferences;
+    ArrayList<String> myStringArray = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_list);
+
+        for (int i = 0; i < sharedPreferences.getInt("size", 0); i++) {
+            myStringArray.add(sharedPreferences.getString("text" + i, "" + i));
+        }
 
         Bundle arguments = getIntent().getExtras();
         String str = arguments.get("hello").toString();
@@ -28,12 +40,14 @@ public class MyList extends Activity {
         Button delete = findViewById(R.id.DeleteButton);
         Button add = findViewById(R.id.AddButton);
 
-        ArrayList<String> myStringArray = new ArrayList<String>();
+
         ArrayAdapter<String> TextAdapter =
                 new ArrayAdapter(this, android.R.layout.simple_list_item_1, myStringArray);
+
         ListView textList = findViewById(R.id.textList);
 
         textList.setAdapter(TextAdapter);
+        
         EditText editText = findViewById(R.id.EditText);
 
         textList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -62,5 +76,21 @@ public class MyList extends Activity {
                 TextAdapter.notifyDataSetChanged();
             }
         });
+    }
+    @Override
+    protected void onDestroy() {
+        sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        for (int i = 0; i < myStringArray.size(); i++)
+        {
+            editor.putString("text" + i, myStringArray.get(i).toString());
+            Log.i("Занесено", myStringArray.get(i).toString());
+            editor.apply();
+        }
+        editor.putInt("size", myStringArray.size());
+        Log.i("size", String.valueOf(myStringArray.size()));
+        editor.apply();
+
+        super.onDestroy();
     }
 }
